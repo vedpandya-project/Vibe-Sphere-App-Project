e8d6efd8c4c35c6ee7c6caf56e8f569a7d56ac1d
vedpandya-project <vedp34678@gmail.com>
2025-04-27 19:43:19 +0530

    Routes and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Button } from "./components/ui/button"
+import { Route, Routes } from "react-router-dom"
+import SigninForm from "./auth/forms/SigninForm"
+import SignupForm from "./auth/forms/SignupForm"
+import Home from "./root/pages/Home"
 
 const App = () => {
   return (
-    <div>
-      <Button variant={"destructive"}>Click Me</Button>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 
