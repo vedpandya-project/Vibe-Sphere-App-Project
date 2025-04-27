67afdd6526316db95b2dcddea76ac46290274ca7
vedpandya-project <vedp34678@gmail.com>
2025-04-27 19:12:32 +0530

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button variant={"destructive"}>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
