36f194084984d3b8215d898bcbdb95b24a8c90ad
vedpandya-project <vedp34678@gmail.com>
2025-04-29 19:32:51 +0530

    Bottombar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { bottombarLinks } from "@/constants"
+import { Link, useLocation } from "react-router-dom"
 
 const Bottombar = () => {
+  const { pathname } = useLocation()
   return (
-    <div>Bottombar</div>
+    <section className='z-50 flex justify-between items-center w-full sticky bottom-0 rounded-t-[20px]
+    px-5 py-4 md:hidden bg-gray-200'>
+      {
+        bottombarLinks.map((link) => {
+          const isActive = pathname === link.route
+
+          return (
+            <Link key={`bottombar-${link.label}`} to={link.route} 
+            className={`${isActive && "bg-blue-400 rounded-[10px]"} flex flex-col
+            justify-center items-center gap-1 p-2 tr{link.imgUrl}sition`}>
+              <img src={link.imgUrl} alt={link.label} width={24} height={24} />
+
+              <p className="text-[14px] font-medium leading-[140%]
+              text-blue-900">{link.label}</p>
+            </Link>
+          )
+        })
+      }
+    </section>
   )
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -29,3 +29,29 @@ export const sidebarLinks = [
         label: "Create Post",
     },
 ]
+
+export const bottombarLinks = [
+    {
+        imgUrl: "https://cdn-icons-png.flaticon.com/128/2163/2163350.png",
+        route: "/",
+        label: "Home"
+    },
+
+    {
+        imgUrl: "https://cdn-icons-png.flaticon.com/128/883/883029.png",
+        route: "/explore",
+        label: "Explore",
+    },
+
+    {
+        imgUrl: "https://cdn-icons-png.flaticon.com/128/5186/5186070.png",
+        route: "/saved",
+        label: "Saved",
+    },
+
+    {
+        imgUrl: "https://cdn-icons-png.flaticon.com/128/10747/10747219.png",
+        route: "/create-post",
+        label: "Create",
+    },
+]
